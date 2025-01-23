4dad82baefd8b5657bdab0027bb67d2539ae3542
spichay <seanpichay1015@gmail.com>
2025-01-23 13:55:12 -0500

    adjust some styling

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,16 +2,16 @@ import {Link} from "react-router-dom";
 
 const Header = () => {
     return (
-        <nav className='h-24 w-full flex items-center justify-center border-b-4 border-black bg-white'>
+        <nav className='h-28 w-full flex items-center justify-center border-b-4 border-black bg-white'>
             <ul className='flex w-full justify-evenly items-center'>
                 <Link to="/products">
-                    <li className='hover:underline text-lg'>Shop</li>
+                    <li className='hover:underline text-lg font-light'>Shop</li>
                 </Link>
                 <Link to="/">
-                    <li className='text-3xl'>Audacieux Atelier</li>
+                    <li className='text-4xl font-light'>Audacieux Atelier</li>
                 </Link>
                 <Link to="/contact">
-                    <li className='hover:underline text-lg'>Contact Us</li>
+                    <li className='hover:underline text-lg font-light'>Contact Us</li>
                 </Link>
             </ul>
         </nav>
