56330ef418932274de3ec155155a5775e38186aa
spichay <seanpichay1015@gmail.com>
2025-01-23 13:53:22 -0500

    finish header component

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,6 +1,20 @@
+import {Link} from "react-router-dom";
+
 const Header = () => {
     return (
-        <h1 className='text-2xl'>test</h1>
+        <nav className='h-24 w-full flex items-center justify-center border-b-4 border-black bg-white'>
+            <ul className='flex w-full justify-evenly items-center'>
+                <Link to="/products">
+                    <li className='hover:underline text-lg'>Shop</li>
+                </Link>
+                <Link to="/">
+                    <li className='text-3xl'>Audacieux Atelier</li>
+                </Link>
+                <Link to="/contact">
+                    <li className='hover:underline text-lg'>Contact Us</li>
+                </Link>
+            </ul>
+        </nav>
     )
 }
 
