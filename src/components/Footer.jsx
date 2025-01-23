2bbe58390b5d3b8b3ae506d450592fd2c93d2edd
spichay <seanpichay1015@gmail.com>
2025-01-23 16:11:19 -0500

    make header responsive and finish footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,8 +1,21 @@
+import {Link} from "react-router-dom";
+
 const Footer = () => {
     return (
-        <>
-            <p>test</p>
-        </>
+        <footer className='flex justify-center w-full h-28 items-center bg-zinc-300'>
+            <ul className='flex flex-row justify-center md:gap-16 w-full min-[320px]:gap-5 min-[320px]:mx-4'>
+                <li className='md:text-lg font-light min-[320px]:text-xs text-center'>&copy; Audacieux Atelier</li>
+                <Link to="https://www.instagram.com">
+                    <li className='md:text-lg font-light min-[320px]:text-xs text-center'>Instagram</li>
+                </Link>
+                <Link to="/contact">
+                    <li className='md:text-lg font-light min-[320px]:text-xs text-center'>Customer Service</li>
+                </Link>
+                <Link to="/contact">
+                    <li className='md:text-lg font-light min-[320px]:text-xs text-center'>Track An Order</li>
+                </Link>
+            </ul>
+        </footer>
     )
 }
 
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,16 +2,16 @@ import {Link} from "react-router-dom";
 
 const Header = () => {
     return (
-        <nav className='h-28 w-full flex items-center justify-center border-b-4 border-black bg-white'>
+        <nav className='h-28 w-full flex items-center justify-center border-b-4 border-black bg-white sticky z-50'>
             <ul className='flex w-full sm:justify-between items-center min-[320px]:justify-center min-[320px]:gap-8 min-[320px]:mx-4 sm:mx-12'>
                 <Link to="/products">
-                    <li className='hover:underline sm:text-lg min-[320px]:lg font-light text-center'>Shop</li>
+                    <li className='hover:underline sm:text-lg min-[320px]:lg font-light text-center min-[320px]:w-16 sm:w-auto min-[440px]:w-28'>Shop</li>
                 </Link>
                 <Link to="/">
                     <li className='sm:text-4xl min-[320px]:text-2xl font-light text-center'>Audacieux Atelier</li>
                 </Link>
                 <Link to="/contact">
-                    <li className='hover:underline sm:text-lg min-[320px]:lg font-light text-center'>Contact Us</li>
+                    <li className='hover:underline sm:text-lg min-[320px]:lg font-light text-center min-[320px]:w-16 sm:w-auto min-[440px]:w-28'>Contact Us</li>
                 </Link>
             </ul>
         </nav>
