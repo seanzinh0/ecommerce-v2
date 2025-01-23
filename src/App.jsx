0ad67ebc2144968d51e2cf9a4e3424713c4a598b
spichay <seanpichay1015@gmail.com>
2025-01-23 13:12:43 -0500

    add images and layout react router

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,23 @@
 import './App.css'
+import {BrowserRouter, Routes, Route} from "react-router-dom";
+import Layout from "./pages/Layout.jsx"
+import Home from "./pages/Home.jsx"
+import Products from "./pages/Products.jsx"
+import Contact from "./pages/Contact.jsx"
 
 function App() {
 
   return (
     <>
+        <BrowserRouter>
+            <Routes>
+                <Route path="/" element={<Layout/>}>
+                    <Route index element={<Home/>}/>
+                    <Route path='/products' element={<Products/>}/>
+                    <Route path='/contact' element={<Contact/>}/>
+                </Route>
+            </Routes>
+        </BrowserRouter>
     </>
   )
 }
