4d3ba265212f8572da5a2950a282c053195c579e
spichay <seanpichay1015@gmail.com>
2025-01-24 12:56:55 -0500

    adjust styling on slider

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -6,7 +6,8 @@ const Home = () => {
     return(
         <>
             <Hero/>
-            <div className='flex justify-center'>
+            <h1 className='text-center mt-20 mb-2 text-2xl md:text-6xl font-light'>Featured Brands</h1>
+            <div className='flex justify-center border-2 border-y-black pb-2'>
             <Slider/>
             </div>
         </>
