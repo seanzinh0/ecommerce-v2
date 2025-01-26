9ba6cb9ecb68a036be0570b31209466bdbf769ce
spichay <seanpichay1015@gmail.com>
2025-01-26 14:48:24 -0500

    change to a grid layout

--- src/components/ProductCard.jsx
+++ src/components/ProductCard.jsx
@@ -11,9 +11,9 @@ function ProductCard() {
 
     return (
         <div className='flex justify-center'>
-        <div className='flex flex-wrap w-10/12 lg:w-full justify-evenly gap-8'>
+        <div className='grid md:grid-rows-6 md:grid-cols-2 w-10/12 gap-5 md:gap-10'>
             {products.map(product => (
-                <div key={product.id} className=" flex flex-col lg:flex-row items-center w-10/12 sm:w-5/12 hover:scale-110 hover:shadow-2xl ease-in-out transition duration-500 shadow-zinc-900 rounded-md">
+                <div key={product.id} className="flex flex-col lg:flex-row items-center w-full hover:scale-110 hover:shadow-2xl ease-in-out transition duration-500 shadow-zinc-900 rounded-md">
                     <img src={product.image} alt={product.title} className='w-10/12 sm:w-8/12 md:w-full lg:w-5/12'/>
                     <div className="flex flex-col gap-2 p-4 h-full w-10/12">
                         <h1 className="text-lg lg:text-2xl text-center">{product.title}</h1>
