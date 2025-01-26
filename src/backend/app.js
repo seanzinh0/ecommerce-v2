d4c738589d76aa61b31935d713087ead6316f990
spichay <seanpichay1015@gmail.com>
2025-01-26 16:08:51 -0500

    adjust stuff for deploying

--- src/backend/app.js
+++ src/backend/app.js
@@ -1,7 +1,12 @@
 import express from 'express'
 import path from 'path'
 import {getProducts} from './utils/dbQueries.js'
+import { fileURLToPath } from 'url';
+import { dirname } from 'path';
 const app = express();
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
 
 const distPath = path.join(__dirname, '../dist')
 app.use(express.static(distPath))
