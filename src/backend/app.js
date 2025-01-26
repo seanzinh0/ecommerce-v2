9593c718c82ee1e7267a3b5a16b218ebd80f73d6
spichay <seanpichay1015@gmail.com>
2025-01-26 15:31:32 -0500

    setup filtering routes and api

--- src/backend/app.js
+++ src/backend/app.js
@@ -1,6 +1,6 @@
 import express from 'express'
 import path from 'path'
-import {getProducts} from './utils/dbQueries.js'
+import {getProducts, filterHoodies, filterPants, filterShirts, filterJackets, filterShoes, filterAccessories} from './utils/dbQueries.js'
 const app = express();
 
 const distPath = path.join(__dirname, '../dist')
@@ -11,6 +11,43 @@ app.get('/api/products', (req, res) => {
         res.send(result)
     })
 })
+
+app.get('/api/products/hoodies', (req, res) => {
+    filterHoodies().then(result => {
+        res.send(result)
+    })
+})
+
+app.get('/api/products/pants', (req, res) => {
+    filterPants().then(result => {
+        res.send(result)
+    })
+})
+
+app.get('/api/products/shirts', (req, res) => {
+    filterShirts().then(result => {
+        res.send(result)
+    })
+})
+
+app.get('/api/products/jackets', (req, res) => {
+    filterJackets().then(result => {
+        res.send(result)
+    })
+})
+
+app.get('/api/products/shoes', (req, res) => {
+    filterShoes().then(result => {
+        res.send(result)
+    })
+})
+
+app.get('/api/products/accessories', (req, res) => {
+    filterAccessories().then(result => {
+        res.send(result)
+    })
+})
+
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
